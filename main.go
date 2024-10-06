70094bcdeba16982acb2e7ba7cdcba405ca403be
rio ten <36088400-rioten353@users.noreply.replit.com>
2024-10-06 14:46:39 +0000

    server started

--- main.go
+++ main.go
@@ -2,8 +2,36 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/rioten353/student_api/internal/config"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	//load config
+
+	cfg := config.MustLoad()
+
+	//setup router
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World"))
+	})
+
+	//setup server
+
+	server := http.Server{
+		Addr:    cfg.Address,
+		Handler: router,
+	}
+
+	fmt.Println("Server is running on", cfg.Address)
+
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal("failed to start server")
+	}
+
 }
